991d48895ffcc924f68ce9ca6b449a50c1b8ec58
Huyang <405586620@qq.com>
2021-03-14 13:29:45 +0800

    feat：列表页和详情页样式

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,52 +1,47 @@
 import React from "react";
-import { Button } from 'antd';
+import { Button } from "antd";
 import "./index.less";
 
-// let img = require('../../static/img/bg.jpg');
-
 const Blog = () => {
+  const location = () => {
+    window.location.hash = "/content";
+  };
   return (
     <div className="blog-wrapper">
       <div className="image-item">
         <div className="nav-title">
-          <div className='image'></div>
-          <p className='nav-text'>胡阳的博客</p>
+          <div className="image"></div>
+          <p className="nav-text">hy的博客</p>
           <p>行稳致远，行稳致远，行稳致远，行稳致远</p>
         </div>
       </div>
       <div className="article">
         <div className="article-item">
-            <div className="article-title">
-                这是标题标题标题
-            </div>
-            <p className="article-content">
-                这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
-            </p>
-            <div className="operate">
-                <Button>阅读全文</Button>
-            </div>
+          <div className="article-title">这是标题标题标题</div>
+          <p className="article-content">
+            这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
+          </p>
+          <div className="operate">
+            <Button onClick={location}>阅读全文</Button>
+          </div>
         </div>
         <div className="article-item">
-            <div className="article-title">
-                这是标题标题标题
-            </div>
-            <p className="article-content">
-                这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
-            </p>
-            <div className="operate">
-                <Button>阅读全文</Button>
-            </div>
+          <div className="article-title">这是标题标题标题</div>
+          <p className="article-content">
+            这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
+          </p>
+          <div className="operate">
+            <Button>阅读全文</Button>
+          </div>
         </div>
         <div className="article-item">
-            <div className="article-title">
-                这是标题标题标题
-            </div>
-            <p className="article-content">
-                这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
-            </p>
-            <div className="operate">
-                <Button>阅读全文</Button>
-            </div>
+          <div className="article-title">这是标题标题标题</div>
+          <p className="article-content">
+            这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
+          </p>
+          <div className="operate">
+            <Button>阅读全文</Button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/content/index.jsx
+++ src/pages/content/index.jsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React from "react";
+import './index.less';
 
 const Content = () => {
-    return <div>it is content</div>
-}
+  return (
+    <div className="content-article">
+      <div className="article-item">
+        <div className="article-title">这是标题标题标题</div>
+        <p className="article-content">
+          这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容这是内容内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          这是内容内容这是内容
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Content;
